5a6655f719df2dfb77e5a0008d0d7b20d6cf7356
jfabiant <john.timoteo@tecsup.edu.pe>
2020-07-10 05:48:30 -0500

    fix controllers api

---
+++ InvoiceAPI/Models/Request/User_Request_v1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvoiceAPI.Models.Request
+{
+    public class User_Request_v1
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int UserTypeID { get; set; }
+
+    }
+}
---
+++ InvoiceAPI/Models/Response/User_Response_v1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvoiceAPI.Models.Response
+{
+    public class User_Response_v1
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int UserTypeID { get; set; }
+    }
+}
